fe3f6a4cf9f81f358d75e9704f501238f2a12920
Tr-nt <tdshanepowell@gmail.com>
2022-12-15 13:09:44 -0500

    Created CRUD functionality for specific products in cars app views file + Registered cars model with Admin site

--- products/views.py
+++ products/views.py
@@ -9,8 +9,31 @@ from .models import Product
 # Create your views here.
 @api_view (['GET','POST'])
 def products_list(request):
-    pass
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
 
 @api_view(['GET','PUT','DELETE'])
 def product_detail(request, pk):
-    pass
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
